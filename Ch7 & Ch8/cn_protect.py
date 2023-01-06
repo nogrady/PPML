34abe4f11a4075aa4ad661ea41b65900a547fbcf
Dumindu Samarweera <samindu@gmail.com>
2023-01-06 11:05:48 -0500

    Add files via upload

---
+++ Ch7 & Ch8/cn_protect.py
@@ -0,0 +1,41 @@
+
+#---------------------------------------------------
+# cn_protect.py
+# Privacy-Preserving Machine Learning
+# Source code for the examples used in the Chapter 7
+# Author: Dumindu Samaraweera 
+#---------------------------------------------------
+
+
+# Section 8.4.3 Implementing k-anonymity in Python
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from cn.protect import Protect
+sns.set(style="darkgrid")
+
+df = pd.read_csv('./Data/all.data.csv')
+
+df.shape
+df.head()
+
+prot = Protect(df)
+
+prot.itypes
+
+prot.itypes.age = 'quasi'
+prot.itypes.sex = 'quasi'
+prot.itypes
+
+
+prot.privacy_model
+
+prot.privacy_model.k = 5
+
+prot.privacy_model
+
+#------------------------------------------------------
+prot_df = prot.protect()
+prot_df
+
---
+++ Ch7 & Ch8/dist_utils.py
@@ -0,0 +1,104 @@
+import pandas as pd
+import numpy as np
+import scipy.stats as st
+import statsmodels as sm
+import warnings
+import matplotlib.pyplot as plt
+
+def best_fit_distribution(data, bins=200, ax=None):
+    """Model data by finding best fit distribution to data"""
+    # Get histogram of original data
+    y, x = np.histogram(data, bins=bins, density=True)
+    x = (x + np.roll(x, -1))[:-1] / 2.0
+
+    # Distributions to check
+    DISTRIBUTIONS = [        
+        st.alpha,st.anglit,st.arcsine,st.beta,st.betaprime,st.bradford,st.burr,st.cauchy,st.chi,st.chi2,st.cosine,
+        st.dgamma,st.dweibull,st.erlang,st.expon,st.exponnorm,st.exponweib,st.exponpow,st.f,st.fatiguelife,st.fisk,
+        st.foldcauchy,st.foldnorm,st.genlogistic,st.genpareto,st.gennorm,st.genexpon,
+        st.genextreme,st.gausshyper,st.gamma,st.gengamma,st.genhalflogistic,st.gilbrat,st.gompertz,st.gumbel_r,
+        st.gumbel_l,st.halfcauchy,st.halflogistic,st.halfnorm,st.halfgennorm,st.hypsecant,st.invgamma,st.invgauss,
+        st.invweibull,st.johnsonsb,st.johnsonsu,st.ksone,st.kstwobign,st.laplace,st.levy,st.levy_l,st.levy_stable,
+        st.logistic,st.loggamma,st.loglaplace,st.lognorm,st.lomax,st.maxwell,st.mielke,st.nakagami,st.ncx2,st.ncf,
+        st.nct,st.norm,st.pareto,st.pearson3,st.powerlaw,st.powerlognorm,st.powernorm,st.rdist,st.reciprocal,
+        st.rayleigh,st.rice,st.recipinvgauss,st.semicircular,st.t,st.triang,st.truncexpon,st.truncnorm,st.tukeylambda,
+        st.uniform,st.vonmises,st.vonmises_line,st.wald,st.weibull_min,st.weibull_max,st.wrapcauchy
+    ]
+
+    # Best holders
+    best_distribution = st.norm
+    best_params = (0.0, 1.0)
+    best_sse = np.inf
+
+    # Estimate distribution parameters from data
+    for idx, distribution in enumerate(DISTRIBUTIONS):
+
+        # Try to fit the distribution
+        try:
+            # Ignore warnings from data that can't be fit
+            with warnings.catch_warnings():
+                warnings.filterwarnings('ignore')
+
+                # fit dist to data
+                params = distribution.fit(data)
+
+                # Separate parts of parameters
+                arg = params[:-2]
+                loc = params[-2]
+                scale = params[-1]
+
+                # Calculate fitted PDF and error with fit in distribution
+                pdf = distribution.pdf(x, loc=loc, scale=scale, *arg)
+                sse = np.sum(np.power(y - pdf, 2.0))
+
+                # if axis pass in add to plot
+                try:
+                    if ax:
+                        pd.Series(pdf, x).plot(ax=ax)
+                except Exception:
+                    pass
+
+                # identify if this distribution is better
+                if best_sse > sse > 0:
+                    best_distribution = distribution
+                    best_params = params
+                    best_sse = sse
+
+        except Exception:
+            pass
+        
+    return (best_distribution.name, best_params)
+
+def make_pdf(dist, params, size=10000):
+    """Generate distributions's Propbability Distribution Function """
+
+    # Separate parts of parameters
+    arg = params[:-2]
+    loc = params[-2]
+    scale = params[-1]
+
+    # Get sane start and end points of distribution
+    start = dist.ppf(0.01, *arg, loc=loc, scale=scale) if arg else dist.ppf(0.01, loc=loc, scale=scale)
+    end = dist.ppf(0.99, *arg, loc=loc, scale=scale) if arg else dist.ppf(0.99, loc=loc, scale=scale)
+
+    # Build PDF and turn into pandas Series
+    x = np.linspace(start, end, size)
+    y = dist.pdf(x, loc=loc, scale=scale, *arg)
+    pdf = pd.Series(y, x)
+
+    return pdf
+
+
+def plot_result(df, continuous, best_distributions):
+    for c, (best_fit_name, best_fit_params) in zip(continuous, best_distributions):
+        best_dist = getattr(st, best_fit_name)
+        pdf = make_pdf(best_dist, best_fit_params)
+        _ = plt.figure(figsize=(12,8))
+        ax = pdf.plot(lw=2, label='PDF', legend=True)
+        _ = df[c].plot(kind='hist', bins=50, normed=True, alpha=0.5, label='Data', legend=True, ax=ax)
+        param_names = (best_dist.shapes + ', loc, scale').split(', ') if best_dist.shapes else ['loc', 'scale']
+        param_str = ', '.join([f'{k}={v:0.2f}' for k,v in zip(param_names, best_fit_params)])
+        dist_str = f'{best_fit_name}({param_str})'
+        _ = ax.set_title(c+ " " + dist_str)
+        _ = ax.set_ylabel('Frequency')
+        plt.show();
